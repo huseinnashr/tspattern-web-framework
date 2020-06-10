15e0465a913d79628d1ae5e78c33335eab3bb9ad
Husein <nvdhunter007@gmail.com>
2020-06-10 22:40:09 +0700

    add user class with data get set

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hi There");
+import { User } from "./models/User";
+
+const user = new User({ name: "myname", age: 20 });
+
+user.set({ name: "newName", age: 999 });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
