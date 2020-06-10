8654a0ebd40b708f31253cb06165b27bc874eb5b
Husein <nvdhunter007@gmail.com>
2020-06-10 23:37:04 +0700

    event handler register and trigger

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,14 @@ const user = new User({ name: "myname", age: 20 });
 
 user.set({ name: "newName", age: 999 });
 
-console.log(user.get("name"));
-console.log(user.get("age"));
+user.on("change", () => {
+  console.log("Change #1");
+});
+user.on("change", () => {
+  console.log("Change #2");
+});
+user.on("save", () => {
+  console.log("Save was triggered");
+});
+
+user.trigger("save");
